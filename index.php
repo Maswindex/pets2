6abb798be2bc0160e88c427d8cba73b02205cba8
Toygan Sevim <tsevim@mail.greenriver.edu>
2018-01-18 17:21:36 -0500

    created forms in view and altered default routes for the forms in index.php

--- index.php
+++ index.php
@@ -11,13 +11,36 @@ require_once("vendor/autoload.php");
 //create an instance of the Base class
 $f3 = Base::instance();
 
-
 //Define a default route
 $f3->route('GET /', function ()
 {
     $view = new View;
     echo $view->render('views/home.html');
 });
+
+//define form1
+$f3->route('GET /pets/order', function ()
+{
+    $view = new View;
+    echo $view->render('views/form1.html');
+});
+
+
+//Define order 2 form 2
+$f3->route('GET /pets/order2', function ()
+{
+    $view = new View;
+    echo $view->render('views/form2.html');
+});
+
+
+//Define a results page
+$f3->route('GET /pets/results', function ()
+{
+    $view = new View;
+    echo $view->render('views/results.html');
+});
+
 
 ///fatfree enable error reporting
 $f3->set('DEBUG', 3); // highest is 3 lowest 0;
