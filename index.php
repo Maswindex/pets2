a0c75b00c698a9a5908e713f3db7b4605cad065d
Toygan Sevim <tsevim@mail.greenriver.edu>
2018-01-18 16:42:44 -0500

    added views directory with home.html page and altered index.php

--- index.php
+++ index.php
@@ -1,15 +1,26 @@
 <?php
-    require_once("vendor/autoload.php");
 
-    //create an instance of the Base class
-    $f3 = Base::instance();
 
-    //Define a default route
-    $f3->route('GET /', function () {
-        $view = new View;
-        echo $view->render
-        ('views/home.php');
-    });
+session_start();
 
-    //run fat free
-    $f3->run();
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+require_once("vendor/autoload.php");
+
+//create an instance of the Base class
+$f3 = Base::instance();
+
+
+//Define a default route
+$f3->route('GET /', function ()
+{
+    $view = new View;
+    echo $view->render('views/home.html');
+});
+
+///fatfree enable error reporting
+$f3->set('DEBUG', 3); // highest is 3 lowest 0;
+
+//run fat free
+$f3->run();
