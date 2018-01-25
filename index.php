e2084f98666fd0a1f5b28b14de13807b6716ed08
Toygan Sevim <tsevim@mail.greenriver.edu>
2018-01-25 17:40:05 -0500

    added a new route and colors arrays and instantiated the repeat in the html file

--- index.php
+++ index.php
@@ -1,49 +1,71 @@
 <?php
-    require_once("vendor/autoload.php");
+/*
+ * Toygan Sevim
+ *
+ */
+require_once("vendor/autoload.php");
 
 
-    //create an instance sof the Base class
-    $f3 = Base::instance();
+//create an instance sof the Base class
 
-    //Define a default route
-    $f3->route('GET /', function () {
-        $view = new View;
-        echo $view->render('views/home.html');
-    });
+$f3 = Base::instance();
 
-    //Define a default route
-    $f3->route('GET /pets/show/@pets', function ($f3, $params) {
-        switch($params['pets']) {
-            case 'dog' :
-                echo '<h2>Dog</h2>';
-                echo "<img src='../../images/dog.jpeg'>";
-                break;
-            case 'cat' :
-                echo '<h2>Cat</h2>';
-                echo "<img src='../../images/cat.jpeg' alt='cat'>";
-                break;
-            default:
-                $f3->error(404);
-        }
-    });
+//colors array
+$f3->set('colors', ['pink', 'green', 'blue']);
 
-    //Define a default route
-    $f3->route('GET /pets/order', function () {
-        $view = new View;
-        echo $view->render('views/form1.html');
-    });
+//Define a default route
+$f3->route('GET /', function ()
+{
+    $view = new View;
+    echo $view->render('views/home.html');
+});
 
-    //Define a default route
-    $f3->route('POST /pets/order2', function () {
-        $view = new View;
-        echo $view->render('views/form2.php');
-    });
+//Define a default route
+$f3->route('GET /pets/show/@pets', function ($f3, $params)
+{
+    switch ($params['pets'])
+    {
+        case 'dog' :
+            echo '<h2>Dog</h2>';
+            echo "<img src='../../images/dog.jpeg'>";
+            break;
+        case 'cat' :
+            echo '<h2>Cat</h2>';
+            echo "<img src='../../images/cat.jpeg' alt='cat'>";
+            break;
+        default:
+            $f3->error(404);
+    }
+});
 
-    //Define a default route
-    $f3->route('POST /pets/results', function () {
-        $view = new View;
-        echo $view->render('views/results.php');
-    });
+//Define a default route
+$f3->route('GET /pets/order', function ()
+{
+    $view = new View;
+    echo $view->render('views/form1.html');
+});
 
-    //run fat free
-    $f3->run();
+//Define a default route
+$f3->route('POST /pets/order2', function ()
+{
+    $view = new View;
+    echo $view->render('views/form2.php');
+});
+
+//Define a default route
+$f3->route('POST /pets/results', function ()
+{
+    $view = new View;
+    echo $view->render('views/results.php');
+});
+
+
+//Define a default route
+$f3->route('GET|POST /new-pet', function ($f3)
+{
+    $view = new View;
+    echo $view->render('views/home.html');
+});
+
+//run fat free
+$f3->run();
